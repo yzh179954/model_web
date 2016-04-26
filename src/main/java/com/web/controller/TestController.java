a5471e949e5a0170b26833c5fc7a1512e388313b
yzh179954 <425273175@qq.com>
2016-04-26 19:08:27 +0800

    complete springmvc+mybaits+spring

--- src/main/java/com/web/controller/TestController.java
+++ src/main/java/com/web/controller/TestController.java
@@ -4,10 +4,15 @@
 package com.web.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.web.bean.User;
+//import com.web.bean.User;
+import com.web.service.UserService;
 
 /**
  * @author  zhihuayang E-mail:425273175@qq.com 
@@ -20,14 +25,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/test")
 public class TestController {
 	
+	@Autowired
+	private UserService userService;
 	
 	private static final Logger log=Logger.getLogger(TestController.class);
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
 	@ResponseBody
-	public String test1(){
-		return "test....";
+	public User test1() throws Exception{
+		log.info("server staring....");
+//		return "sssssss";
+		return userService.getUserById(1);
 	}
 
 }
---
+++ src/main/java/com/web/service/UserService.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.web.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.web.bean.User;
+//import com.web.dao.UserDao;
+import com.web.dao.UserMapper;
+
+/**
+ * @author zhihuayang E-mail:425273175@qq.com
+ * @date 创建时间：2016年4月25日 下午4:48:42
+ * @version 1.0
+ * @parameter
+ * @return
+ */
+
+@Service
+public class UserService {
+
+	 @Autowired
+	 private UserMapper userMapper;
+
+	public User getUserById(Integer id) {
+//		User user = new User();
+//		user.setId(1);
+//		user.setPassword("ssssssssss");
+//		user.setUsername("yzh");
+//		return userDao.getById(1);
+		return userMapper.selectByPrimaryKey(id);
+//		return user;
+	}
+
+}
